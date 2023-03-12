17a9a9c5e858185f1eda043520d16609649776e4
Anastaciya Dasiukevich <nastya_d1199@mail.ru>
2023-03-12 09:51:08 +0300

    Add tests for login into product page. Move func of login to base page

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,7 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 import math
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -11,6 +12,17 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def should_be_login_url(self):
+        url = self.browser.current_url
+        assert 'login' in url, 'no substring "login" in url'
 
     def is_element_present(self, how, what):
         try:
